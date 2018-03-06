0393503408618dc8dc74198f2ce79392be705917
jinyuanzhen <jinyuanzhen@shanghai3h.com>
2018-03-06 18:01:25 +0800

    在view1页面搭建sui基本样式

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -17,9 +17,27 @@ angular.module('myApp.view1', ['ngRoute'])
 // 'use strict';
 
 // angular.module('myApp.view1', ['ngRoute'])
-    .controller('View1Ctrl', ['$scope','$state',
-        function($scope,$state) {
-        	// $state.go("home");
-        	console.log('view1Ctrl');
+.controller('View1Ctrl', ['$scope', '$state',
+    function($scope, $state) {
+        // $state.go("home");
+        // console.log('view1Ctrl');
+        // setTimeout(function() {
+        //     console.log('hello world');
+        //     setBlockHeight();
+        // }, 0);
+
+        function setBlockHeight() {
+            for (var i = 0; i < $('li').length; i++) {
+                var blockWidth = $('li')[i].clientWidth;
+                $('li').height(blockWidth / 2);
+            }
+            // for(var i=0;i<$('.index_block').length;i++){
+            //  console.log(i);
+            //  var blockWidth=$('.index_block')[i].clientWidth;
+            //  $('.index_block')[i].style.height=blockWidth;
+            // }
         }
-    ])
+        setBlockHeight();
+
+    }
+])
